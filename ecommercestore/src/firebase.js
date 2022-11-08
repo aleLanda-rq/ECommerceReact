2d3fa41b630bd5287f0e59f31c23760af80c0a37
Alejandra <aleriquene@gmail.com>
2022-11-08 14:38:38 -0600

    home page done

---
+++ ecommercestore/src/firebase.js
@@ -0,0 +1,10 @@
+ // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyA505VAQPMmcUvvRbE5kT4XWPUhoCAooR0",
+    authDomain: "store-3aeac.firebaseapp.com",
+    projectId: "store-3aeac",
+    storageBucket: "store-3aeac.appspot.com",
+    messagingSenderId: "939961140676",
+    appId: "1:939961140676:web:1e3a04578498ca776f1091",
+    measurementId: "G-6PCQ5V0FT6"
+  };
